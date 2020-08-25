c71f256fe2d14a3458a7ac6d3a889d40ee479d0e
atkuzmanov <atkuzmanov@gmail.com>
2020-08-25 16:45:02 +0300

    Work-In-Progress-WIP-2020-08-25-164502

--- download-entire-website-wget-script-v2.0.sh
+++ download-entire-website-wget-script-v2.0.sh
@@ -15,14 +15,17 @@ DOMAIN=""
 DOMAIN_STRIPPED=""
 URL_PROTOCOL_STRIPPED=""
 TARGET_DOWNLOAD_DIR_NAME=""
+TARGET_DOWNLOAD_SUBDIR=""
 ################################
 extract_domain_name() {
     DOMAIN=$(sed -E -e 's_.*://([^/@]*@)?([^/:]+).*_\2_' <<< "$URL")
+    echo "--> DOMAIN: ${DOMAIN}"
 }
 # extract_domain_name
 ################################
 strip_www_from_domain_name() {
     DOMAIN_STRIPPED=$(echo "$DOMAIN" | sed "s/^www\.//")
+    echo "--> DOMAIN_STRIPPED: ${DOMAIN_STRIPPED}"
 }
 # strip_www_from_domain_name
 ################################
@@ -47,6 +50,8 @@ strip_protocol_from_domain_name() {
     # f=${f%%/*}
 
     URL_PROTOCOL_STRIPPED="$f"
+
+    echo "--> URL_PROTOCOL_STRIPPED: ${URL_PROTOCOL_STRIPPED}"
 }
 # strip_protocol_from_domain_name
 ################################
@@ -54,17 +59,34 @@ generate_target_download_dir_name () {
     # TARGET_DOWNLOAD_DIR_NAME=$(echo $URL_PROTOCOL_STRIPPED | sed 's@/@@g')
     temp1=${URL_PROTOCOL_STRIPPED////$'_'}
     temp2=${temp1//./$'-'}
-    TARGET_DOWNLOAD_DIR_NAME="$HOME/Downloads/downloaded-websites-wget/$temp2"
+    
+    if [[ -z "$TARGET_DOWNLOAD_SUBDIR" ]] ; then
+        TARGET_DOWNLOAD_DIR_NAME="$HOME/Downloads/downloaded-websites-wget/$temp2"
+    else
+        TARGET_DOWNLOAD_DIR_NAME="$HOME/Downloads/downloaded-websites-wget/$TARGET_DOWNLOAD_SUBDIR/$temp2"
+    fi
+    echo "--> TARGET_DOWNLOAD_DIR_NAME: ${TARGET_DOWNLOAD_DIR_NAME}"
 }
 # generate_target_download_dir_name
 ################################
 download_entire_website_wget() {
+    ##---
     ## add `--mirror` option to
     ## Turn on options suitable for mirroring. This option turns on recursion and time-stamping, 
     ## sets infinite recursion depth and keeps FTP directory listings. It is currently equivalent to ‘-r -N -l inf --no-remove-listing’.
+    ##---
+    ## `--no-clobber` clashes with `--convert-links` and `--convert-links` tales precedence, so use one or the other.
+    ## use `--quiet` or `--no-verbose` as necessary
+    ##---
+    ## `wget --user uname --password pass $url || true;` This way, if wget fails, the result of that line is still zero and your script continues.
+    ## https://stackoverflow.com/questions/29723144/how-to-resume-loop-when-wget-returns-error-404-bash
+    ##---
 
     wget \
-    --quiet \
+    --content-on-error=on \
+    --tries=0 \
+    --no-check-certificate \
+    --no-verbose \
     --page-requisites \
     --span-hosts \
     --convert-links \
@@ -74,23 +96,30 @@ download_entire_website_wget() {
     --random-wait \
     --user-agent=Mozilla \
     --continue \
-    --no-clobber \
     --directory-prefix="$TARGET_DOWNLOAD_DIR_NAME" \
     --domains ${DOMAIN_STRIPPED} \
-    ${URL}
+    ${URL} || true;
 }
 # download_entire_website_wget
 ################################
 download_for_each_website_wget_from_file() {
     while IFS= read -r line
     do
-        URL="$line"
-        extract_domain_name
-        strip_www_from_domain_name
-        strip_protocol_from_domain_name
-        generate_target_download_dir_name
-        download_entire_website_wget
-        sleep 1 ;
+        if [[ $line = \#* ]] ; then
+            temp3="${line/\#[[:space:]]/}"
+            TARGET_DOWNLOAD_SUBDIR="${temp3//[[:space:]]/$'-'}"
+            echo "--> TARGET_DOWNLOAD_SUBDIR: $TARGET_DOWNLOAD_SUBDIR"
+            continue
+        else        
+            URL="$line"
+            echo "--> URL: ${URL}"
+            extract_domain_name
+            strip_www_from_domain_name
+            strip_protocol_from_domain_name
+            generate_target_download_dir_name
+            download_entire_website_wget
+            sleep 1 ;
+        fi
     done < "$FILE"
     echo "Done."
 }
@@ -122,10 +151,15 @@ download_for_each_website_wget_from_file
 ## https://www.cyberciti.biz/faq/get-extract-domain-name-from-url-in-linux-unix-bash/
 ## https://stackoverflow.com/questions/2871181/replacing-some-characters-in-a-string-with-another-character
 ## https://unix.stackexchange.com/questions/272596/replace-character-x-with-character-y-in-a-string-with-bash
-## 
 ## https://stackoverflow.com/questions/10929453/read-a-file-line-by-line-assigning-the-value-to-a-variable
 ## https://www.cyberciti.biz/faq/unix-howto-read-line-by-line-from-file/
 ## https://askubuntu.com/questions/411540/how-to-get-wget-to-download-exact-same-web-page-html-as-browser
 ## https://superuser.com/questions/1150495/save-an-entire-webpage-with-all-images-and-css-into-just-one-folder-and-one-fi
 ## https://www.puzzle.ch/de/blog/articles/2018/02/12/phantomjs-is-dead-long-live-headless-browsers
+## https://stackoverflow.com/questions/29723144/how-to-resume-loop-when-wget-returns-error-404-bash
+## https://stackoverflow.com/questions/27658675/how-to-remove-last-n-characters-from-a-string-in-bash
+## https://stackoverflow.com/questions/28256178/how-can-i-match-spaces-with-a-regexp-in-bash/28256343
+## https://unix.stackexchange.com/questions/78625/using-sed-to-find-and-replace-complex-string-preferrably-with-regex
+## https://www.cyberciti.biz/faq/unix-linux-bash-script-check-if-variable-is-empty/
+## https://stackoverflow.com/questions/13509508/check-if-string-is-neither-empty-nor-space-in-shell-script
 ################################
